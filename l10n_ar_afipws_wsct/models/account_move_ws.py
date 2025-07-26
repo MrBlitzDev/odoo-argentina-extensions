979df70c8d91b1694d7578875d0f3de052629f8d
RodrigoDiazGarin <rodrigo.diazg@davinci.edu.ar>
2025-07-26 16:47:47 -0300

    Factura T

--- l10n_ar_afipws_wsct/models/account_move_ws.py
+++ l10n_ar_afipws_wsct/models/account_move_ws.py
@@ -1,16 +1,25 @@
 from decimal import Decimal
-from odoo import models
+from odoo import _, models
+from odoo.exceptions import UserError
+from datetime import datetime
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
     def wsct_request_autorization(self, ws):
-        ws.CAESolicitar()
+        cae = ws.CAESolicitar()
+        if (cae):
+            ws_date_str = ws.Vencimiento
+            parsed_date = datetime.strptime(ws_date_str, "%Y/%m/%d")
+            formatted_date = parsed_date.strftime("%Y%m%d")
+            ws.Vencimiento = formatted_date
 
     def wsct_map_invoice_info(self):
         invoice_info = self.base_map_invoice_info()
 
         amounts = invoice_info["amounts"]
+
+        invoice_info["lines"] = self.wsct_invoice_map_info_lines()
 
         invoice_info["imp_reintegro"] = str("%.2f" % -amounts["vat_amount"])
         invoice_info["imp_subtotal"] = self.amount_untaxed
@@ -19,9 +28,24 @@ class AccountMove(models.Model):
         invoice_info["fecha_cbte"] = invoice_info["fecha_cbte"].strftime("%Y-%m-%d")
         invoice_info["domicilio"] = invoice_info["commercial_partner"].contact_address_inline
         invoice_info["cod_relacion"] = 1
-        invoice_info["observaciones"] = 'Observaciones: preguntar que enviamos'
+        invoice_info["observaciones"] = False
 
         return invoice_info
+    
+    def wsct_invoice_add_info(self, ws, invoice_info):        
+        for line in invoice_info["lines"]:
+
+            ws.AgregarItem(
+                line["item_type_t"], 
+                line["cod_tur"], 
+                line["codigo"],
+                line["ds"], 
+                line["iva_id"],
+                line["imp_iva"],
+                line["importe"]
+            )
+
+        self.pyafipws_add_tax(ws)
     
     def wsct_pyafipws_create_invoice(self, ws, invoice_info):
         ws.CrearFactura(
@@ -47,3 +71,57 @@ class AccountMove(models.Model):
             invoice_info["observaciones"],
             invoice_info["cancela_misma_moneda_ext"],
         )
+
+    def wsct_invoice_map_info_lines(self):
+        lines = []
+        for line in self.invoice_line_ids.filtered(lambda x: x.display_type == 'product'):
+            line_temp = {}
+            line_temp["codigo"] = line.product_id.default_code
+            # unidad de referencia del producto si se comercializa
+            # en una unidad distinta a la de consumo
+            # uom is not mandatory, if no UOM we use "unit"
+            if not line.product_uom_id:
+                line_temp["umed"] = "7"
+            elif not line.product_uom_id.l10n_ar_afip_code:
+                raise UserError(
+                    _("Not afip code con producto UOM %s" % (line.product_uom_id.name))
+                )
+            else:
+                line_temp["umed"] = line.product_uom_id.l10n_ar_afip_code
+            # cod_mtx = line.uom_id.l10n_ar_afip_code
+            line_temp["ds"] = line.name
+            line_temp["qty"] = line.quantity
+            line_temp["precio"] = line.price_unit            
+            # calculamos bonificacion haciendo teorico menos importe
+            line_temp["bonif"] = (
+                line.discount
+                and str(
+                    "%.2f"
+                    % (line_temp["precio"] * line_temp["qty"] - line_temp["importe"])
+                )
+                or None
+            )
+            tax_lines = line.tax_ids.filtered(lambda x: not x.l10n_ar_afipws_wsct_is_tourism_vat)
+            line_temp["iva_id"] = tax_lines.tax_group_id.l10n_ar_vat_afip_code
+            vat_taxes_amounts = tax_lines.compute_all(
+                line.price_unit,
+                self.currency_id,
+                line.quantity,
+                product=line.product_id,
+                partner=self.partner_id,
+            )
+            
+            vat_amount = sum(
+                [x["amount"] for x in vat_taxes_amounts["taxes"]]
+            )
+
+            line_temp["imp_iva"] = "%.2f" % vat_amount
+            line_temp["importe"] = "%.2f" % (line.price_total + vat_amount)
+
+            # Factura T
+            line_temp["item_type_t"] = line.product_id.categ_id.item_type_t
+            line_temp["cod_tur"] = line.product_id.categ_id.cod_tur
+
+            lines.append(line_temp)
+
+        return lines
